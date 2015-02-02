95d9c7e0f21b71812267672948fe829991a57bd5
Jeremy Hennig <jeremy.hennig@ualberta.ca>
2015-02-02 13:16:37 -0700

    removed redirect and put in error message

--- main.php
+++ main.php
@@ -80,11 +80,13 @@ $session_id = "";
 		
 	
       //There is no session in the database matching your session id... send to login page.
-      header('Location: ' . $MACEWAN_LOGIN_PAGE);
+     	echo "Error a session ID was invalid.";
+		exit();
     }
   } else {
     //There is no session id cookie or argument... send to login page.
-    header('Location: ' . $MACEWAN_LOGIN_PAGE);
+   	echo "Error a session ID was invalid.";
+	exit();
   }
   
 ?>  
